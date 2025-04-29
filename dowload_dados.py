c281d6acb0c63ac31a44a93725e423a608f1d570
ketlinvkbs <vitoriavkbs@gmail.com>
2025-04-29 12:41:23 -0300

    Update dowload_dados.py

--- dowload_dados.py
+++ dowload_dados.py
@@ -1,5 +1,6 @@
 import basedosdados as bd
 
+bd.config.save_credentials(path_to_credentials="CAMINHO/credentials.json")
 bd.download(
     dataset_id="basedosdados.br_inpe_queimadas.microdados",
     billing_project_id="devrapido-458115",#id projeto google cloud
